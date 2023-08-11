b24744ae531671eb063ae6c36e048abd75eba33f
Rhaz05 <laurenrodrigueza9@gmail.com>
2023-08-11 11:26:21 +0800

    Added bmssdb data and added add script for access

--- routes/access.js
+++ routes/access.js
@@ -1,9 +1,81 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql = require('./repository/bmssdb');
+const helper = require('./repository/customhelper');
+const dictionary = require('./repository/dictionary');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('access');
 });
 
 module.exports = router;
+
+router.get('/load', (req, res) => {
+  try {
+      let sql = `select * from master_access_type`;
+
+      mysql.Select(sql, 'MasterAccessType', (err, result) => {
+          if (err) {
+              return res.json({
+                  msg: err
+              })
+          }
+
+          console.log(helper.GetCurrentDatetime());
+
+          res.json({
+              msg: 'success',
+              data: result
+          })
+      });
+  } catch (error) {
+      res.json({
+          msg: error
+      })
+  }
+})
+
+router.post('/save', (req, res) => {
+  try {
+      let accessname = req.body.accessname;
+      let status = dictionary.GetValue(dictionary.ACT());
+      let createdby = "Ralph Lauren Santos";
+      let createdate = helper.GetCurrentDatetime();
+      let data = [];
+
+      let sql_check = `select * from master_access_type where mat_accessname='${accessname}'`;
+
+      mysql.Select(sql_check, 'MasterAccessType', (err, result) => {
+          if (err) console.error('Error: ', err);
+
+          if (result.length != 0) {
+              return res.json({
+              msg: 'exist'
+              })
+          }else {
+              data.push([
+                  accessname,
+                  status,
+                  createdby,
+                  createdate
+              ])
+      
+              mysql.InsertTable('master_access_type', data, (err, result) => {
+                  if (err) console.error('Error: ', err);
+      
+                  console.log(result);
+      
+                  res.json({
+                      msg: 'success',
+                  })
+              })
+          }
+      })
+  }catch (error) {
+      res.json({
+          msg: error
+      })
+  }
+})
--- routes/repository/bmssdb.js
+++ routes/repository/bmssdb.js
@@ -11,7 +11,6 @@ crypt.Decrypter(process.env._PASSWORD, (err, result) => {
     console.log(`${result}`);
 });
 
-
 const connection = mysql.createConnection({
     host: process.env._HOST,
     user: process.env._USER,
@@ -39,4 +38,321 @@ exports.CheckConnection = () => {
         }
         console.log('MySQL database connection established successfully!');
     });
+}
+
+exports.CheckConnection = () => {
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connection to MYSQL databases: ', err);
+            return;
+        }
+        console.log('MySQL database connection established successfully!');
+    });
+}
+
+exports.InsertMultiple = async (stmt, todos) => {
+    try {
+        connection.connect((err) => { return err; })
+        // console.log(statement: ${stmt} data: ${todos});
+
+        connection.query(stmt, [todos], (err, results, fields) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`Row inserted: ${results.affectedRows}`);
+
+            return 1;
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.Select = (sql, table, callback) => {
+    try {
+        connection.connect((err) => { return err; })
+        connection.query(sql, (error, results, fields) => {
+ 
+            // console.log(results);
+
+            if (error) {
+                callback(error, null)
+            }
+
+            if (table == 'MasterAccessType') {
+                callback(null, model.MasterAccessType(results));
+            }
+
+            if (table == 'MasterPositionType') {
+                callback(null, model.MasterPositionType(results));
+            }
+
+            if (table == 'MasterUser') {
+                callback(null, model.MasterUser(results));
+            }
+
+            if (table == 'MasterEmployees') {
+                callback(null, model.MasterEmployees(results));
+            }
+
+            if (table == 'MasterProduct') {
+                callback(null, model.MasterProduct(results));
+            }
+
+            if (table == 'MasterPos') {
+                callback(null, model.MasterPos(results));
+            }
+
+            if (table == 'MasterBranch') {
+                callback(null, model.MasterBranch(results));
+            }
+
+            if (table == 'EmployeeGovernmentIdDetails') {
+                callback(null, model.EmployeeGovernmentIdDetails(results));
+            }
+
+            if (table == 'EmployeeDeductionDetails') {
+                callback(null, model.EmployeeDeductionDetails(results));
+            }
+
+            if (table == 'EmployeeAllowance') {
+                callback(null, model.EmployeeAllowance(results));
+            }
+
+            if (table == 'EmployeeSalary') {
+                callback(null, model.EmployeeSalary(results));
+            }
+
+            if (table == 'PayrollDetail') {
+                callback(null, model.PayrollDetail(results));
+            }
+
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.StoredProcedure = (sql, data, callback) => {
+    try {
+
+        connection.query(sql, data, (error, results, fields) => {
+            if (error) {
+                callback(error.message, null);
+            }
+            callback(null, results[0])
+        });
+    } catch (error) {
+        callback(error, null);
+    }
+}
+
+exports.StoredProcedureResult = (sql, callback) => {
+    try {
+
+        connection.query(sql, (error, results, fields) => {
+            if (error) {
+                callback(error.message, null);
+            }
+            callback(null, results[0])
+        });
+    } catch (error) {
+        callback(error, null);
+    }
+}
+
+exports.Update = async (sql, callback) => {
+    try {
+        connection.query(sql, (error, results, fields) => {
+            if (error) {
+                callback(error, null)
+            }
+            // console.log('Rows affected:', results.affectedRows);
+
+            callback(null, `Rows affected: ${results.affectedRows}`);
+        });
+    } catch (error) {
+        callback(error, null)
+    }
+}
+
+exports.UpdateMultiple = async (sql, data, callback) => {
+    try {
+        connection.query(sql, data, (error, results, fields) => {
+            if (error) {
+                callback(error, null)
+            }
+            // console.log('Rows affected:', results.affectedRows);
+
+            callback(null, `Rows affected: ${results.affectedRows}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.CloseConnect = () => {
+    connection.end();
+}
+
+exports.Insert = (stmt, todos, callback) => {
+    try {
+        connection.connect((err) => { return err; })
+        // console.log(statement: ${stmt} data: ${todos});
+
+        connection.query(stmt, [todos], (err, results, fields) => {
+            if (err) {
+                callback(err, null);
+            }
+            // callback(null, Row inserted: ${results});
+            callback(null, `Row inserted: ${results.affectedRows}`);
+            // console.log(Row inserted: ${results.affectedRows});
+        });
+
+    } catch (error) {
+        callback(error, null);
+    }
+}
+
+exports.SelectResult = (sql, callback) => {
+    try {
+        connection.connect((err) => { return err; })
+        connection.query(sql, (error, results, fields) => {
+
+            // console.log(results);
+
+            if (error) {
+                callback(error, null)
+            }
+
+            callback(null, results);
+
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.InsertTable = (tablename, data, callback) => {
+    if (tablename == 'master_access_type') {
+        let sql = `INSERT INTO master_access_type(
+            mat_accessname,
+            mat_status,
+            mat_createdby,
+            mat_createddate) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+ 
+    if (tablename == 'master_position_type') {
+        let sql = `INSERT INTO master_position_type(
+            mpt_positioncode,
+            mpt_positionname,
+            mpt_status,
+            mpt_createdby,
+            mpt_createddate) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_user') {
+        let sql = `INSERT INTO master_user(
+            mu_employeeid,
+            mu_accesstype,
+            mu_positiontype,
+            mu_status,
+            mu_createdby,
+            mu_createddate) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_employees') {
+        let sql = `INSERT INTO master_employees(
+            me_employeeid,
+            me_fullname,
+            me_position,
+            me_contactinfo,
+            me_datehired,
+            me_status,
+            me_createdby,
+            me_createddate) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_product') {
+        let sql = `INSERT INTO master_product(
+            mp_productid,
+            mp_description,
+            mp_status,
+            mp_createdby,
+            mp_createddate) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_pos') {
+        let sql = `INSERT INTO master_pos(
+            mp_posid,
+            mp_posname,
+            mp_serial,
+            mp_min,
+            mp_ptu,
+            mp_status,
+            mp_createdby,
+            mp_createddate) VALUES ?`;
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_branch') {
+        let sql = `INSERT INTO master_branch(
+            mb_branchid,
+            mb_branchname,
+            mb_tin,
+            mb_address,
+            mb_logo,
+            mb_status,
+            mb_createdby,
+            mb_createddate) VALUES ?`;
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
 }
