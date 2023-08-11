f384511663da343cc3a5a7f92b680c647d601df1
Rhaz05 <laurenrodrigueza9@gmail.com>
2023-08-11 09:55:05 +0800

    Setup for Database

---
+++ routes/repository/bmssdb.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql');
+const model = require('../model/payrollmodel');
+require('dotenv').config();
+const crypt = require('./cryptography');
+
+let password = '';
+crypt.Decrypter(process.env._PASSWORD, (err, result) => {
+    if (err) throw err;
+
+    password = result;
+    console.log(`${result}`);
+});
+
+
+const connection = mysql.createConnection({
+    host: process.env._HOST,
+    user: process.env._USER,
+    password: password,
+    database: process.env._DATABASE
+});
+
+crypt.Encrypter('#Ebedaf19dd0d', (err, result) => {
+    if (err) console.error('Error: ', err);
+
+    console.log(result);
+})
+
+// crypt.Decrypter('f6a3287039d0d75cb83cb29d35b3dfcb', (err, result) => {
+//     if (err) console.error('Error: ', err);
+
+//     console.log(${result});
+// });
+
+exports.CheckConnection = () => {
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connection to MYSQL databases: ', err);
+            return;
+        }
+        console.log('MySQL database connection established successfully!');
+    });
+}
