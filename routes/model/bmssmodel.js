e8a3c61176a2566b8baefaaed5f0b021aa864db7
Rhaz05 <laurenrodrigueza9@gmail.com>
2023-08-30 13:17:02 +0800

    Added table Model to bmssmodel and bmssdb based on excel

--- routes/model/bmssmodel.js
+++ routes/model/bmssmodel.js
@@ -143,6 +143,56 @@ exports.MasterBranch = (data) => {
   return dataResult;
 };
 
+exports.MasterVendor = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      vendorid: key.mv_vendorid,
+      verndorname: key.mv_vendorname,
+      status: key.mv_status,
+      createdby: key.mv_createdby,
+      createddate: key.mv_createddate,
+    });
+  });
+
+  return dataResult;
+};
+
+exports.MasterLocation = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      locationid: key.ml_locationid,
+      locationname: key.ml_locationname,
+      status: key.ml_status,
+      createdby: key.ml_createdby,
+      createddate: key.ml_createddate,
+    });
+  });
+
+  return dataResult;
+};
+
+exports.MasterMaterialCost = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      materialid: key.mmc_materialid,
+      materialname: key.mmc_materialname,
+      unitcost: key.mmc_unitcost,
+      unit: key.mmc_unit,
+      status: key.mmc_status,
+      createdby: key.mmc_createdby,
+      createddate: key.mmc_createddate
+    });
+  });
+
+  return dataResult;
+};
+
 exports.SalesDetail = (data) => {
   let dataResult = [];
 
@@ -277,6 +327,271 @@ exports.UserInfo = (data) => {
       accesstype: key.mu_accesstype,
       positiontype: key.mu_positiontype,
       status: key.mu_status,
+    });
+  });
+
+  return dataResult;
+};
+
+exports.SystemLogs = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      logid: key.sl_logid,
+      logdate: key.sl_logdate,
+      loglevel: key.sl_loglevel,
+      source: key.sl_source,
+      message: key.sl_message,
+      userid: key.sl_userid,
+      ipaddress: key.sl_ipaddress
+    });
+  });
+
+  return dataResult;
+};
+
+exports.PurchaseOrder = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      orderid: key.po_orderid,
+      vendorid: key.po_vendorid,
+      orderdate: key.po_orderdate,
+      deliverydate: key.po_deliverydate,
+      amount: key.po_total_amount,
+      paymentterms: key.po_paymentterms,
+      deliverymethod: key.po_deliverymethod,
+      status: key.po_status,
+    });
+  });
+
+  return dataResult;
+};
+
+exports.PurchaseOrderItems = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      productid: key.poi_productid,
+      orderid: key.poi_orderid,
+      description: key.poi_description,
+      quantity: key.poi_quantity,
+      unitprice: key.poi_unitprice,
+      totalprice: key.poi_totalprice
+    });
+  });
+
+  return dataResult;
+};
+
+exports.TransferOrders = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      transferid: key.to_transferid,
+      fromlocationid: key.to_fromlocationid,
+      tolocationid: key.to_tolocationid,
+      orderdate: key.to_orderdate,
+      expecteddate: key.to_expecteddate,
+      totalquantity: key.to_totalquantity,
+      status: key.to_status,
+      notes: key.to_notes      
+    });
+  });
+
+  return dataResult;
+};
+
+exports.TransferOrdersItems = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      itemid: key.toi_itemid,
+      transferid: key.toi_transferid,
+      productid: key.toi_productid,
+      quantity: key.toi_quantity
+    });
+  });
+
+  return dataResult;
+};
+
+exports.InventoryCount = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      countid: key.ic_countid,
+      countdate: key.ic_count_date,
+      locationid: key.ic_locationid,
+      countby: key.ic_countby,
+      countverification: key.ic_countverification,
+      notes: key.ic_notes      
+    });
+  });
+
+  return dataResult;
+};
+
+exports.InventoryCountItems = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      itemid: key.ici_itemid,
+      countid: key.ici_countid,
+      productid: key.ici_productid,
+      countedby: key.ici_countedby,
+      actualquantity: key.ici_actualquantity,
+      variance: key.ici_variance          
+    });
+  });
+
+  return dataResult;
+};
+
+exports.Production = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      productionid: key.p_productionid,
+      productid: key.p_productid,
+      startdate: key.p_startdate,
+      enddate: key.p_enddate,
+      quantityproduced: key.p_quantityproduced,
+      productiononline: key.p_productionline,
+      supervisorid: key.p_supervisorid,
+      notes: key.p_notes
+    });
+  });
+
+  return dataResult;
+};
+
+exports.ProductionActivities = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      avtivityid: key.pa_activityid,
+      productionid: key.pa_productionid,
+      activityname: key.pa_activityname,
+      startdate: key.pa_startdate,
+      enddate: key.pa_enddate,
+      workerid: key.pa_workerid
+    });
+  });
+
+  return dataResult;
+};
+
+exports.ProductionComponents = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      componentid: key.pc_componentid,
+      productid: key.pc_productid,
+      details: key.pc_details,
+      totalcost: key.pc_totalcost,
+      status: key.pc_status,
+      createdby: key.pc_createdby,
+      createddate: key.pc_createddate
+    });
+  });
+
+  return dataResult;
+};
+
+exports.InventoryHistory = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      historyid: key.ih_historyid,
+      productid: key.ih_productid,
+      transactiondate: key.ih_transactiondate,
+      transactiontype: key.ih_transactiontype,
+      previousquantity: key.ih_previousquantity,
+      changeinquantity: key.ih_changeinquantity,
+      newquantity: key.ih_newquantity,
+      notes: key.ih_notes
+    });
+  });
+
+  return dataResult;
+};
+
+exports.StockAdjustment = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      adjustmentid: key.sa_adjustmentid,
+      productid: key.sa_productid,
+      adjustmentdate: key.sa_adjustmentdate,
+      adjustmenttype: key.sa_adjustmenttype,
+      quantityadjusted: key.sa_quantityadjusted,
+      reason: key.sa_reason,
+      adjustedby: key.sa_adjustedby,
+      notes: key.sa_notes,
+    });
+  });
+
+  return dataResult;
+};
+
+exports.InventoryValuationReport = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      reportid: key.ivr_reportid,
+      reportdate: key.ivr_reportdate,
+      generatedby: key.ivr_generateby,
+      notes: key.ivr_notes
+    });
+  });
+
+  return dataResult;
+};
+
+exports.InventoryValuationItems = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      itemid: key.ivi_itemid,
+      reportid: key.ivi_reportid,
+      productid: key.ivi_productid,
+      quantity: key.ivi_quantity,
+      unitcost: key.ivi_unitcost,
+      totalvalue: key.ivi_totalvalue      
+    });
+  });
+
+  return dataResult;
+};
+
+exports.LabelPrinting = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      labelid: key.lp_labelid,
+      itemname: key.lp_itemname,
+      itemcode: key.lp_itemcode,
+      quantity: key.lp_quantity,
+      labeltype: key.lp_labeltype,
+      printdate: key.lp_printdate,
+      printby: key.lp_printby,
+      notes: key.lp_notes
     });
   });
 
