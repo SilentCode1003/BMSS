62616625524dd6a670e5be8670dedf6c334111ac
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-10-14 10:36:39 +0800

    Revise getcategory API on Product Price Module

--- routes/model/model.js
+++ routes/model/model.js
@@ -1,36 +1,58 @@
 //#region  SQL DATA MODEL
 class ProductPriceModel {
-    constructor(productpriceid,
-        productid,
-        description,
-        barcode,
-        productimage,
-        price,
-        category,
-        previousprice,
-        pricechange,
-        pricechangedate,
-        status,
-        createdby,
-        createddate) {
-        this.productpriceid = productpriceid;
-        this.productid = productid;
-        this.description = description;
-        this.barcode = barcode;
-        this.productimage = productimage;
-        this.price = price;
-        this.category = category;
-        this.previousprice = previousprice;
-        this.pricechange = pricechange;
-        this.pricechangedate = pricechangedate;
-        this.status = status;
-        this.createdby = createdby;
-        this.createddate = createddate;
-    }
+  constructor(
+    productpriceid,
+    productid,
+    description,
+    barcode,
+    productimage,
+    price,
+    category,
+    previousprice,
+    pricechange,
+    pricechangedate,
+    status,
+    createdby,
+    createddate
+  ) {
+    this.productpriceid = productpriceid;
+    this.productid = productid;
+    this.description = description;
+    this.barcode = barcode;
+    this.productimage = productimage;
+    this.price = price;
+    this.category = category;
+    this.previousprice = previousprice;
+    this.pricechange = pricechange;
+    this.pricechangedate = pricechangedate;
+    this.status = status;
+    this.createdby = createdby;
+    this.createddate = createddate;
+  }
+}
+
+class ProductCategory {
+  constructor(
+    productid,
+    description,
+    barcode,
+    productimage,
+    price,
+    category,
+    quantity
+  ) {
+    this.productid = productid;
+    this.description = description;
+    this.barcode = barcode;
+    this.productimage = productimage;
+    this.price = price;
+    this.category = category;
+    this.quantity = quantity;
+  }
 }
 //#endregion
 
-
 module.exports = {
-    ProductPriceModel
-}
+  ProductPriceModel,
+  ProductCategory,
+};
