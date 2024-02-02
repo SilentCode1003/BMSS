6b73f7a6729b8f7a18438de40e3fe4fa10af9d2b
Rhaz05 <laurenrodrigueza9@gmail.com>
2024-02-02 11:50:18 +0800

    [notification]-[separate the scripts of notification]-[finished the ui and added close for notification with inclusion of sound when there's new notification]

--- routes/salesdetails.js
+++ routes/salesdetails.js
@@ -13,6 +13,45 @@ router.get("/", function (req, res, next) {
 
 module.exports = router;
 
+router.post("/close", (req, res) => {
+  try {
+    let notifid = req.body.notifid;
+    let status = "CLOSED";
+
+    let data = [status, notifid];
+
+    let sql_Update = `UPDATE notification 
+                       SET n_status = ?
+                       WHERE n_id = ?`;
+
+    let sql_check = `SELECT * FROM notification WHERE n_id='${notifid}'`;
+
+    mysql.Select(sql_check, "Notification", (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      if (result.length != 1) {
+        return res.json({
+          msg: "notexist",
+        });
+      } else {
+        mysql.UpdateMultiple(sql_Update, data, (err, result) => {
+          if (err) console.error("Error: ", err);
+
+          console.log(result);
+
+          res.json({
+            msg: "success",
+          });
+        });
+      }
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
 router.post("/load", (req, res) => {
   try {
     const shift = req.body.shift;
