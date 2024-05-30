c654f0334fb30d68c1127359fd7624f30ebc0c5a
Rhaz05 <laurenrodrigueza9@gmail.com>
2024-05-30 13:28:48 +0800

    Minor adjustment
    -Adjustment in production material unit selection
    -Update display of production history

--- routes/productionhistory.js
+++ routes/productionhistory.js
@@ -1,38 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const mysql = require('./repository/bmssdb');
-const helper = require('./repository/customhelper');
-const dictionary = require('./repository/dictionary');
+const mysql = require("./repository/bmssdb");
+const helper = require("./repository/customhelper");
+const dictionary = require("./repository/dictionary");
 const { Validator } = require("./controller/middleware");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    Validator(req, res, "productionhistory");
+  Validator(req, res, "productionhistory");
 });
 
 module.exports = router;
 
-router.get('/load', (req, res) => {
-    try {
-        let sql = `SELECT * FROM production_history`;
+router.get("/load", (req, res) => {
+  try {
+    let sql = `SELECT ph_historyid AS id, ph_productionid AS productionId, ph_quantity AS quantity, mp_description AS productName, me_fullname as supervisor
+        FROM production_history 
+        INNER JOIN production ON p_productionid = ph_productionid 
+        INNER JOIN master_product ON mp_productid = p_productid
+        INNER JOIN master_employees ON me_employeeid = p_supervisorid`;
 
-        mysql.Select(sql, 'ProductionHistory', (err, result) => {
-            if (err) {
-                console.log(err)
-                return res.json({
-                    msg: err
-                })
-            }
-            res.json({
-                msg: 'success',
-                data: result
-            })
+    mysql.SelectResult(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.json({
+          msg: err,
         });
-    } catch (error) {
-        res.json({
-            msg: error
-        })
-    }
-})
-
+      }
+      res.json({
+        msg: "success",
+        data: result,
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
