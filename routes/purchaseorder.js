5a94a5431d553f9407fb0ccb6db1909af263e6ba
Rhaz05 <laurenrodrigueza9@gmail.com>
2024-07-18 14:43:33 +0800

    Update
    -Added sent email for production

--- routes/purchaseorder.js
+++ routes/purchaseorder.js
@@ -1,264 +1,242 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const crypto = require('crypto');
-const mysql = require('./repository/bmssdb');
-const helper = require('./repository/customhelper');
-const dictionary = require('./repository/dictionary');
-const { Validator } = require("./controller/middleware");
+const crypto = require('crypto')
+const mysql = require('./repository/bmssdb')
+const helper = require('./repository/customhelper')
+const dictionary = require('./repository/dictionary')
+const { Validator } = require('./controller/middleware')
+const { SelectAll, Query, Transaction, Check } = require('./utility/query.util')
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-    Validator(req, res, "purchaseorder");
-});
+router.get('/', function (req, res, next) {
+  Validator(req, res, 'purchaseorder')
+})
 
-module.exports = router;
+module.exports = router
 
-
-router.get('/load', (req, res) => {
+router.get('/load', async (req, res) => {
   try {
-      let sql = `SELECT po_orderid as po_orderid, mv_vendorname as po_vendorid, po_orderdate as po_orderdate, po_deliverydate as po_deliverydate,
-          po_total_amount as po_total_amount, po_paymentterms as po_paymentterms, po_deliverymethod as po_deliverymethod, po_status po_status
-          FROM salesinventory.purchase_order
-          INNER JOIN master_vendor on mv_vendorid = po_vendorid
-          ORDER BY po_orderid DESC;`;
+    const loadPurchaseOrder = `SELECT po_orderid as po_orderid, mv_vendorname as po_vendorid, po_orderdate as po_orderdate, po_deliverydate as po_deliverydate,
+    po_total_amount as po_total_amount, po_paymentterms as po_paymentterms, po_deliverymethod as po_deliverymethod, po_status po_status
+    FROM salesinventory.purchase_order
+    INNER JOIN master_vendor on mv_vendorid = po_vendorid
+    ORDER BY po_orderid DESC;`
 
-      mysql.Select(sql, 'PurchaseOrder', (err, result) => {
-          if (err) {
-              return res.json({
-                  msg: err
-              })
-          }
+    const response = await Query(loadPurchaseOrder, [], 'po_')
 
-          console.log(helper.GetCurrentDatetime());
-
-          res.json({
-              msg: 'success',
-              data: result
-          })
-      });
+    res.status(200).json({
+      msg: 'success',
+      data: response,
+    })
   } catch (error) {
-      res.json({
-          msg: error
-      })
+    res.status(400).json({
+      msg: error,
+    })
   }
 })
 
 router.post('/save', (req, res) => {
   try {
-      let vendorid = req.body.vendorid;
-      let orderdate = req.body.orderdate;
+    const { vendorid, orderdate, deliverydate, totalamount, paymentterms, deliverymethod } =
+      req.body
 
-      
-      let deliverydate = req.body.deliverydate;
-      let totalamount = req.body.totalamount;
-      let paymentterms = req.body.paymentterms;
-      let deliverymethod = req.body.deliverymethod;
-      let status = dictionary.GetValue(dictionary.PND());
-      let data = [];
+    let status = dictionary.GetValue(dictionary.PND())
+    let data = []
 
-      data.push([
-        vendorid,
-        orderdate,
-        deliverydate,
-        totalamount,
-        paymentterms,
-        deliverymethod,
-        status
-      ])
+    data.push([
+      vendorid,
+      orderdate,
+      deliverydate,
+      totalamount,
+      paymentterms,
+      deliverymethod,
+      status,
+    ])
 
-      // console.log(data, "Data")
+    // console.log(data, "Data")
 
-      mysql.InsertTable('purchase_order', data, (err, result) => {
-          if (err) console.error('Error: ', err);
-          let purchaseid = result[0]["id"];
-          let poiData = JSON.parse(req.body.poiData);
-          // console.log(poiData);
-          poiData.forEach(function(item, index) {
-              let description = item.description;
-              let quantity = item.quantity;
-              let unitprice = item.unitprice;
-              let totalprice = item.totalprice;
+    mysql.InsertTable('purchase_order', data, (err, result) => {
+      if (err) console.error('Error: ', err)
+      let purchaseid = result[0]['id']
+      let poiData = JSON.parse(req.body.poiData)
+      // console.log(poiData);
+      poiData.forEach(function (item, index) {
+        let description = item.description
+        let quantity = item.quantity
+        let unitprice = item.unitprice
+        let totalprice = item.totalprice
 
-              let rowData = [
-                purchaseid,
-                description,
-                quantity,
-                unitprice,
-                totalprice
-              ];
-        
-            //  console.log(rowData);
-              mysql.InsertTable('purchase_order_items', [rowData], (err, result) => {
-                if (err) console.error('Error: ', err);
-                // console.log(result)
-              })
-          });
+        let rowData = [purchaseid, description, quantity, unitprice, totalprice]
 
-          res.json({
-              msg: 'success',
-          })
+        //  console.log(rowData);
+        mysql.InsertTable('purchase_order_items', [rowData], (err, result) => {
+          if (err) console.error('Error: ', err)
+          // console.log(result)
+        })
       })
-  } catch (error) {
+
       res.json({
-          msg: error
+        msg: 'success',
       })
+    })
+  } catch (error) {
+    res.json({
+      msg: error,
+    })
   }
 })
 
-router.post("/approve", (req, res) => {
-    try {
-      let orderid = req.body.orderid;
-      let status =
+router.post('/approve', (req, res) => {
+  try {
+    let orderid = req.body.orderid
+    let status =
       req.body.status == dictionary.GetValue(dictionary.PND())
         ? dictionary.GetValue(dictionary.APD())
-        : dictionary.GetValue(dictionary.PND());
-      let data = [status, orderid];
-      console.log(data);
-  
-      let sql_Update = `UPDATE purchase_order 
-                       SET po_status = ?
-                       WHERE po_orderid = ?`;
-  
-      mysql.UpdateMultiple(sql_Update, data, (err, result) => {
-        if (err) console.error("Error: ", err);
-  
-        res.json({
-          msg: "success",
-        });
-      });
-    } catch (error) {
+        : dictionary.GetValue(dictionary.PND())
+    let data = [status, orderid]
+    console.log(data)
+
+    let sql_Update = `UPDATE purchase_order SET po_status = ? WHERE po_orderid = ?`
+
+    mysql.UpdateMultiple(sql_Update, data, (err, result) => {
+      if (err) console.error('Error: ', err)
+
       res.json({
-        msg: error,
-      });
-    }
-});
-  
-router.post("/cancel", (req, res) => {
+        msg: 'success',
+      })
+    })
+  } catch (error) {
+    res.json({
+      msg: error,
+    })
+  }
+})
+
+router.post('/cancel', (req, res) => {
   try {
-    let orderid = req.body.orderid;
+    let orderid = req.body.orderid
     let status =
-    req.body.status == dictionary.GetValue(dictionary.PND())
-      ? dictionary.GetValue(dictionary.CND())
-      : dictionary.GetValue(dictionary.PND());
-    let data = [status, orderid];
-    console.log(data);
+      req.body.status == dictionary.GetValue(dictionary.PND())
+        ? dictionary.GetValue(dictionary.CND())
+        : dictionary.GetValue(dictionary.PND())
+    let data = [status, orderid]
+    console.log(data)
 
     let sql_Update = `UPDATE purchase_order 
                       SET po_status = ?
-                      WHERE po_orderid = ?`;
+                      WHERE po_orderid = ?`
 
     mysql.UpdateMultiple(sql_Update, data, (err, result) => {
-      if (err) console.error("Error: ", err);
+      if (err) console.error('Error: ', err)
 
       res.json({
-        msg: "success",
-      });
-    });
+        msg: 'success',
+      })
+    })
   } catch (error) {
     res.json({
       msg: error,
-    });
+    })
   }
-});
+})
 
-router.post("/completed", (req, res) => {
+router.post('/completed', (req, res) => {
   try {
-    let orderid = req.body.orderid;
-    let status = dictionary.GetValue(dictionary.CMP());
-    let data = [status, orderid];
-    console.log(data);
+    let orderid = req.body.orderid
+    let status = dictionary.GetValue(dictionary.CMP())
+    let data = [status, orderid]
+    console.log(data)
 
     let sql_Update = `UPDATE purchase_order 
                       SET po_status = ?
-                      WHERE po_orderid = ?`;
+                      WHERE po_orderid = ?`
 
     mysql.UpdateMultiple(sql_Update, data, (err, result) => {
-      if (err) console.error("Error: ", err);
+      if (err) console.error('Error: ', err)
 
       res.json({
-        msg: "success",
-      });
-    });
+        msg: 'success',
+      })
+    })
   } catch (error) {
     res.json({
       msg: error,
-    });
+    })
   }
-});
+})
 
 router.get('/loadsampleitem', (req, res) => {
   try {
-      let sql = `select * from sample_itemlists`;
-
-      mysql.Select(sql, 'SampleItemLists', (err, result) => {
-          if (err) {
-              return res.json({
-                  msg: err
-              })
-          }
-
-          console.log(helper.GetCurrentDatetime());
-
-          res.json({
-              msg: 'success',
-              data: result
-          })
-      });
-  } catch (error) {
-      res.json({
-          msg: error
-      })
-  }
-});
-
-router.post('/getitemdetails', (req, res) => {
-  try {
-    let description = req.body.description;
-    let sql = `select * from sample_itemlists where sil_description = '${description}'`;
+    let sql = `select * from sample_itemlists`
 
     mysql.Select(sql, 'SampleItemLists', (err, result) => {
       if (err) {
-          return res.json({
-              msg: err
-          })
+        return res.json({
+          msg: err,
+        })
+      }
+
+      console.log(helper.GetCurrentDatetime())
+
+      res.json({
+        msg: 'success',
+        data: result,
+      })
+    })
+  } catch (error) {
+    res.json({
+      msg: error,
+    })
+  }
+})
+
+router.post('/getitemdetails', (req, res) => {
+  try {
+    let description = req.body.description
+    let sql = `select * from sample_itemlists where sil_description = '${description}'`
+
+    mysql.Select(sql, 'SampleItemLists', (err, result) => {
+      if (err) {
+        return res.json({
+          msg: err,
+        })
       }
       res.json({
-          msg: 'success',
-          data: result
+        msg: 'success',
+        data: result,
       })
-    });
-    } catch (error) {
-      res.json({
-          msg: error
-      })
-    }
-});
+    })
+  } catch (error) {
+    res.json({
+      msg: error,
+    })
+  }
+})
 
 router.post('/getorderdetails', (req, res) => {
   try {
-    let orderid = req.body.orderid;
+    let orderid = req.body.orderid
     let sql = `select poi_productid as productid, poi_orderid as orderid, mpm_productname as description, poi_quantity as quantity, 
               poi_unitprice as unitprice, poi_totalprice as totalprice, poi_description as materialid
               from purchase_order_items 
               INNER JOIN production_materials ON mpm_productid = poi_description 
-                where poi_orderid = '${orderid}'`;
+                where poi_orderid = '${orderid}'`
 
     mysql.SelectResult(sql, (err, result) => {
       if (err) {
-          return res.json({
-              msg: err
-          })
+        return res.json({
+          msg: err,
+        })
       }
       // console.log(result)
       res.json({
-          msg: 'success',
-          data: result
+        msg: 'success',
+        data: result,
       })
-    });
-    } catch (error) {
-      res.json({
-          msg: error
-      })
-    }
-});
+    })
+  } catch (error) {
+    res.json({
+      msg: error,
+    })
+  }
+})
--- routes/repository/mailer.js
+++ routes/repository/mailer.js
@@ -1,40 +1,40 @@
-const nodemailer = require("nodemailer");
-const { Decrypter } = require("./cryptography");
+const nodemailer = require('nodemailer')
+const { Decrypter } = require('./cryptography')
 
-let password = "";
-Decrypter("334381f4aa236941a6d80e6b57eee75d", (err, encryted) => {
-  if (err) console.error("Error: ", err);
-  console.log(encryted);
-  password = encryted;
-});
+let password = ''
+Decrypter('334381f4aa236941a6d80e6b57eee75d', (err, encryted) => {
+  if (err) console.error('Error: ', err)
+  console.log(encryted)
+  password = encryted
+})
 
 // Create a Nodemailer transporter with custom SMTP settings
 const transporter = nodemailer.createTransport({
-  host: "mail.5lsolutions.com", // Your mail server hostname or IP address
+  host: 'mail.5lsolutions.com', // Your mail server hostname or IP address
   port: 587, // Your mail server port (587 is the default for secure SMTP)
   secure: false, // Set to true if your server uses SSL/TLS, otherwise false
   auth: {
-    user: "5lpos@5lsolutions.com",
+    user: '5lpos@5lsolutions.com',
     pass: password,
   },
-});
+})
 
 exports.SendEmail = (to, subject, text) => {
   // Email content
   const mailOptions = {
-    from: "5lpos@5lsolutions.com",
+    from: '5lpos@5lsolutions.com',
     to: to,
     subject: subject,
     html: text,
-  };
+  }
 
   // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error(error);
+      console.error(error)
     } else {
-      console.log(info);
-      console.log("Email sent: " + info.response);
+      console.log(info)
+      console.log('Email sent: ' + info.response)
     }
-  });
-};
+  })
+}
