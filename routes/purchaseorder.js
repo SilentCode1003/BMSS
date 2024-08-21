1ee932f515772a5b5a57eb7fa9ab2ece6f25e8d1
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2024-08-21 12:09:07 +0800

    Revise Query Purchase Order

--- routes/purchaseorder.js
+++ routes/purchaseorder.js
@@ -16,11 +16,18 @@ module.exports = router
 
 router.get('/load', async (req, res) => {
   try {
-    const loadPurchaseOrder = `SELECT po_orderid as po_orderid, mv_vendorname as po_vendorid, po_orderdate as po_orderdate, po_deliverydate as po_deliverydate,
-    po_total_amount as po_total_amount, po_paymentterms as po_paymentterms, po_deliverymethod as po_deliverymethod, po_status po_status
-    FROM salesinventory.purchase_order
-    INNER JOIN master_vendor on mv_vendorid = po_vendorid
-    ORDER BY po_orderid DESC`
+    const loadPurchaseOrder = `SELECT po_orderid as po_orderid, 
+                              mv_vendorname as po_vendorid, 
+                              po_orderdate as po_orderdate, 
+                              po_deliverydate as po_deliverydate,
+                              po_total_amount as po_total_amount, 
+                              po_paymentterms as po_paymentterms, 
+                              po_deliverymethod as po_deliverymethod, 
+                              po_status po_status
+                              FROM salesinventory.purchase_order
+                              INNER JOIN master_vendor on mv_vendorid = po_vendorid
+                              WHERE NOT po_status IN ('COMPLETED','CANCELLED') 
+                              ORDER BY po_orderid DESC`
 
     const response = await Query(loadPurchaseOrder, [], 'po_')
 
